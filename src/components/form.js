221488fc0a2265190fca225ec62194514bb8adfd
torianne02 <victoria.fluharty@gmail.com>
2019-02-25 10:19:09 -0800

    adds text boxes and button for form

--- src/components/form.js
+++ src/components/form.js
@@ -3,7 +3,27 @@ import { Container, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Form = props => {
   return (
-
+    <Container className="form">
+      <Form onSubmit={ props.handleOnSubmit }>
+        <FormGroup>
+          <Label for="city" hidden>City</Label>
+          <Input type="text"
+            name="city"
+            placeholder="City"
+            onChange={ props.handleOnChange }
+            value={ props.cityValue } />
+        </FormGroup>
+        <FormGroup>
+          <Label for="country" hidden>Country</Label>
+          <Input type="text"
+            name="country"
+            placeholder="Country"
+            onChange={ props.handleOnChange }
+            value={ props.countryValue } />
+        </FormGroup>
+        <Button>Get Weather</Button>
+      </Form>
+    </Container>
   )
 }
 
