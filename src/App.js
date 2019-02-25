624462c5564fa8b60febc155c46a963e5401ae8b
torianne02 <victoria.fluharty@gmail.com>
2019-02-25 09:49:24 -0800

    adds router and switch via react-router-dom to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+  return (
+    <Router>
       <div className="App">
-        Weather Caster
+        <Switch>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
