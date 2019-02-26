1baae7e4f0a1824b8a46980f205d74b43ace7b44
torianne02 <victoria.fluharty@gmail.com>
2019-02-26 10:52:59 -0800

    changes styling of weather displayed removes use of CardText, CardTitle, CardSubtitle, etc. incorprotes use of p and span tags adds styling on App.css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Card, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card } from 'reactstrap';
 
 const Weather = ( props ) =>
-  <div className="weather">
+  <div className="weather-info">
     <Card>
-      <CardBody>
-        <CardTitle>{props.city}</CardTitle>
-        <CardText>Current Temperature: {props.temperature}&deg;</CardText>
-        <CardText>Wind: {props.wind} mph</CardText>
-        <CardText>Humidity: {props.humidity}%</CardText>
-        <CardText>Conditions: {props.description}</CardText>
-      </CardBody>
+      <p className="weather-key">
+        Location: <span className="weather-value">{ props.city }</span>
+      </p>
+      <p className="weather-key">
+        Temperature: <span className="weather-value">{ props.temperature }&deg;</span>
+      </p>
+      <p className="weather-key">
+        Wind: <span className="weather-value">{ props.wind } mph</span>
+      </p>
+      <p className="weather-key">
+        Humidity: <span className="weather-value">{ props.humidity }%</span>
+      </p>
+      <p className="weather-key">
+        Conditions: <span className="weather-value">{ props.description }</span>
+      </p>
     </Card>
   </div>
 
