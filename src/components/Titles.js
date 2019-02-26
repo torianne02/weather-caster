9f6a1d06e5e7ca836929d353af9824e5d53a3662
torianne02 <victoria.fluharty@gmail.com>
2019-02-26 11:09:56 -0800

    creates Titles component

---
+++ src/components/Titles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Titles = () =>
+  <div className="titles-component">
+    <h1 className="title">Weather Caster</h1>
+    <p className="subtitle">Find the current weather conditions in any city.</p>
+  </div>
+
+export default Titles
